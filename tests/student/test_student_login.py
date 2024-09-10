6addcc4be559feb04de2e7b5702c386d4d27a85d
segunUb18 <eeomo@yahoo.com>
2024-09-10 04:25:16 +0100

    :bug: fix import related issue

--- tests/student/test_student_login.py
+++ tests/student/test_student_login.py
@@ -1,5 +1,8 @@
 import pytest
+import os
+import sys
 import json
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from unittest.mock import patch, MagicMock
 from app import create_app, db
 from api.v2.models import Student
